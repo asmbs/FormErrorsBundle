fa9a3d79e6d545bf67f6b5c8cf6031e264ef581e
Cody LaFlamme <cody@asmbs.org>
2023-02-15 12:22:35 -0500

    Update TranslatorInterface namespace for symfony 5

--- Services/FormErrorsParser.php
+++ Services/FormErrorsParser.php
@@ -4,27 +4,27 @@ namespace Ex3v\FormErrorsBundle\Services;
 
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * This software is provided as-is on the terms of MIT License. 
+ * This software is provided as-is on the terms of MIT License.
  * Feel free to use, modify and share in ground of both non-commercial and commercial projects.
- * 
+ *
  * What it does?
  * This service travels through all levels of form and gathers errors from every element.
  * Then it returns errors for you as array of errors - form errors, subforms errors and fields errors.
- * Notice that you need this approach to get ALL errors from form - also form (not fields) errors itself, 
+ * Notice that you need this approach to get ALL errors from form - also form (not fields) errors itself,
  * as well as errors from custom validators you have created.
- * 
- * 
+ *
+ *
  * Returned data is array of Symfony\Component\Form\FormError objects (default) or localized messages (use setReturnAsString() method)
- * It can be flat (one level) array (easier to iterate) as well as multidimensional array 
+ * It can be flat (one level) array (easier to iterate) as well as multidimensional array
  * that represents the structure of your form (easier to join form elements with errors).
- * 
+ *
  * Why do I need this?
  * I wrote this service to be able to quickly get errors from any form and display them in one place.
  * Now it is my pleasure to share my code with you.
- * 
+ *
  * @author Maciej Szkamruk <ex3v@ex3v.com>
  */
 class FormErrorsParser
@@ -37,6 +37,7 @@ class FormErrorsParser
      * @param TranslatorInterface $translator
      */
     public function __construct(TranslatorInterface $translator){
+
         $this->translator = $translator;
     }
 
@@ -63,12 +64,12 @@ class FormErrorsParser
      */
     private function realParseErrors(FormInterface $form, array &$results)
     {
-        
+
         /*
          * first check if there are any errors bound for this element
          */
         $errorsIterator = $form->getErrors();
-        
+
         if(count($errorsIterator)){
 
             $config = $form->getConfig();
@@ -97,21 +98,21 @@ class FormErrorsParser
                 'errors' => $errorMessages
             );
         }
-        
+
         /*
          * Then, check if there are any children. If yes, then parse them
          */
-        
+
         $children = $form->all();
 
         if(count($children)){
             foreach($children as $child){
                 if($child instanceof FormInterface){
-                    $this->realParseErrors($child, $results);                   
+                    $this->realParseErrors($child, $results);
                 }
             }
         }
-        
+
         return $results;
     }
 
